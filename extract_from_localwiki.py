8e99f5c86aebf33db9aab6384feb4e37f319d44b
Andrew M. Kelly <a.m.kelly@mind-gloaming.net>
2024-02-24 13:08:10 -0500

    added some error handling & code for fetching files json data

--- extract_from_localwiki.py
+++ extract_from_localwiki.py
@@ -9,32 +9,48 @@ import json
 localwiki_api_url = "https://localwiki.org/api/v4/"
 
 responses = []
-initial_url = f"{localwiki_api_url}pages/?region=23"
-file_path = 'data/responses1.json'
+initial_pages_url = f"{localwiki_api_url}pages/?region=23"
+initial_files_url = f"{localwiki_api_url}files/?region=23"
+pages_file_path = 'data/pages_responses.json'
+files_file_path = 'data/files_responses.json'
 
-def fetch_data_from_localwiki(pages_url):
+def fetch_data_from_localwiki(url):
     """
     Fetches data from the LocalWiki API for a specific region.
     """
   
     # Send a GET request to the LocalWiki API for the specific region
     try:
-            response = requests.get(pages_url)
+            response = requests.get(url)
             response.raise_for_status()  # Raises stored HTTPError, if one occurred.
     except requests.exceptions.RequestException as err:
-            print(f"Error fetching data from LocalWiki API for {pages_url}: {err}")
+            print(f"Error fetching data from LocalWiki API for {url}: {err}")
             return None
         
     print(response.url)
     return response.json()  # Return JSON response
 
-next_page_url = initial_url
-while next_page_url is not None:
-    data = fetch_data_from_localwiki(next_page_url)
+next_pages_url = initial_pages_url
+while next_pages_url is not None:
+    data = fetch_data_from_localwiki(next_pages_url)
     if data is not None:
-        with open(file_path, 'a') as outfile:
+        with open(pages_file_path, 'a') as outfile:
             json.dump(data, outfile)
             outfile.write('\n')  # Newline separates each JSON object for readability
-        next_page_url = data['next']  # Update the next page URL for the next iteration
+        next_pages_url = data['next']  # Update the next page URL for the next iteration
     else:
-        break  # Exit the loop if fetching data fails
+        time.wait(1)
+        data = fetch_data_from_localwiki(next_files_url)
+
+
+next_files_url = initial_files_url
+while next_files_url is not None:
+    data = fetch_data_from_localwiki(next_files_url)
+    if data is not None:
+        with open(files_file_path, 'a') as outfile:
+            json.dump(data, outfile)
+            outfile.write('\n')  # Newline separates each JSON object for readability
+        next_files_url = data['next']  # Update the next page URL for the next iteration
+    else:
+        time.wait(1)
+        data = fetch_data_from_localwiki(next_files_url)
